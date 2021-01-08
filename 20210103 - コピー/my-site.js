1c0e7e190db1cdf4f6be04e2fee4db2e3bc4b36a
Masatsuguyamamoto <75605639+Masatsuguyamamoto@users.noreply.github.com>
2021-01-09 00:02:06 +0900

    Add files via upload
    2021/1/10 new up

---
+++ 20210103 - コピー/my-site.js
@@ -0,0 +1,22 @@
+
+
+
+
+
+
+
+
+
+
+
+
+// $(function() {
+// 	$('body').fadeIn(1000); //1秒かけてフェードイン！
+// });
+
+
+// var images = new Array();
+
+// var randomNum = Math.floor(Math.random()*10);
+
+// document.write("<img src=" +images[randomNum]+">" );
